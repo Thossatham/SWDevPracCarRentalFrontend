2743614211bcbdb97d5d557aa5b8b293a134073b
Thossatham <thossatham1@gmail.com>
2025-03-22 14:54:22 +0700

    debug building issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['drive.google.com'],
-    },
-    experimental: {
-      serverActions: true
-    },
-    env: {
-      FRONTEND_URL: process.env.FRONTEND_URL,
-      BACKEND_URL: process.env.BACKEND_URL
-    }
-  }
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['drive.google.com'],
+  },
+  experimental: {
+    serverActions: true, // Enable Server Actions
+  },
+  env: {
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    BACKEND_URL: process.env.BACKEND_URL,
+  },
+};
+
+export default nextConfig;
